af49faeda736e9ff1888bf7a3b274b4fa4ca064b
Roman Soltan <romabandit88@gmail.com>
2025-01-10 00:17:26 +0200

    delete css, modified app, main.jsx

--- module-1/lesson-1/src/App.jsx
+++ module-1/lesson-1/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// .jsx вказує на те, що у файлі є розмітка js.
+// Все, що ми пишемо в App, перетворюється в розмітку
+// і вставляється в root.
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <header>
+        <div>logo</div>
+        <nav>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </nav>
+      </header>
 
-export default App
+      <section>
+        <h2>Welcome to React!!!</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident et
+          quaerat, architecto voluptatem repudiandae, veritatis commodi odit,
+          praesentium veniam ducimus vitae eaque? Neque accusamus in, maiores
+          itaque excepturi assumenda ratione? Blanditiis, hic eum officia libero
+          repudiandae, facere velit odit obcaecati odio laudantium laborum non
+          quaerat, quas veritatis cupiditate debitis explicabo quidem sit
+          nesciunt fuga. Modi sit magnam reiciendis a eos!
+        </p>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+      </section>
+
+      <footer>
+        <span>&copy; All right reserved 2025</span>
+      </footer>
+    </div>
+  );
+};
+
+// App маємо експортувати
+export default App;
--- module-1/lesson-1/src/main.jsx
+++ module-1/lesson-1/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// Початковий файл, з якого все починається.
+// Створюється в одному екземплярі
+// Рендерить App
 
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
