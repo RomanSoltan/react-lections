c05fcb8af709c4d9d48384f2e811868f92f20848
Roman Soltan <romabandit88@gmail.com>
2025-02-07 06:28:08 +0200

    nested route

--- module-5/lection-9/src/components/App.jsx
+++ module-5/lection-9/src/components/App.jsx
@@ -3,6 +3,9 @@ import Header from "./Header/Header";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import NotFound from "../pages/NotFound";
+import Aim from "./NestedRoutes/Aim";
+import Company from "./NestedRoutes/Company";
+import Team from "./NestedRoutes/Team";
 
 function App() {
   return (
@@ -10,7 +13,11 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<About />}>
+          <Route path="aim" element={<Aim />} />
+          <Route path="company" element={<Company />} />
+          <Route path="team" element={<Team />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </main>
