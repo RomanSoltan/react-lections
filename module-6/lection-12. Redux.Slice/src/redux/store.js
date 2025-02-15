d709548ec9eea0b90fbfe297303c2e69c5ec9fbe
Roman Soltan <romabandit88@gmail.com>
2025-02-15 07:05:40 +0200

    persist

--- module-6/lection-12. Redux.Slice/src/redux/store.js
+++ module-6/lection-12. Redux.Slice/src/redux/store.js
@@ -1,10 +1,45 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterReducer } from "./counterSlice";
+// підключення persist
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "counter-persist",
+  version: 1,
+  storage,
+  // blacklist for localStorage
+  // blacklist: ["step"],
+  // відкидається все окрім counter
+  whitelist: ["counter"],
+};
+
+// трикутник долі для persist
+const persistedReducer = persistReducer(persistConfig, counterReducer);
 
 export const store = configureStore({
   // пiдключаємо reducer
   // 7. Підключити в сторі новий слайс замість старого редюсера
   reducer: {
-    counter: counterReducer,
+    counter: persistedReducer,
   },
+  // для persist
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+// export persistor
+export const persistor = persistStore(store);
